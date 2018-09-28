d91854938b4498ce5b6c054e9c8df147fb32aa5a
AudioStakes <audiostakes@gmail.com>
2018-09-28 23:49:02 +0900

    Constantsクラスを作成して定数を管理できるようにした

---
+++ CVESummaryGenerator/Constants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace CVESummaryGenerator
+{
+    public static class Constants
+    {
+        public static class ColumnName
+        {
+            public static readonly string CveNumber = "CVE";
+            public static readonly string CveTitle = "概要";
+            public static readonly string Description = "詳細";
+            public static readonly string PubliclyDisclosed = "一般に公開";
+            public static readonly string Exploited = "悪用";
+            public static readonly string LatestReleaseExploitability = "最新のソフトウェア リリース";
+            public static readonly string OlderReleaseExploitability = "過去のソフトウェア リリース";
+            public static readonly string VectorString = "CVSS";
+            public static readonly string BaseScore = "BaseScore";
+            public static readonly string TemporalScore = "TemporalScore";
+            public static readonly string Severity = "深刻度";
+            public static readonly string Remarks = "備考";
+        }
+
+        public static class ProductName
+        {
+            public static readonly string Win_2008_32Bit_SP2 = "Windows Server 2008 for 32-bit Systems Service Pack 2";
+            public static readonly string Win_2012_R2_SeverCore = "Windows Server 2012 R2 (Server Core installation)";
+            public static readonly string Win_2016_ServerCore = "Windows Server 2016  (Server Core installation)";
+        }
+    }
+}
